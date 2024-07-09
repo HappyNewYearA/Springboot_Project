783304ab5b7051fb40b1a3777d103950f8e932b0
mowang-mw <mowang@whu.edu.cn>
2024-07-09 22:12:37 +0800

    Springboot_Project

---
+++ HappyNewYearA/HappyNewYearA/src/main/java/com/happynewyeara/HappyNewYearAApplication.java
@@ -0,0 +1,12 @@
+package com.happynewyeara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyNewYearAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyNewYearAApplication.class, args);
+	}
+}
